75fc7c75f1544c93ef8a83bebbb8c0f37babe085
bofu <bofusoci@gmail.com>
2022-02-21 21:29:10 +0800

    init

---
+++ app.js
@@ -0,0 +1,130 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({extended: false}) );
+
+var cors = require("cors");
+app.use(cors());
+
+// 以 express-session 管理狀態資訊
+var session = require('express-session');
+app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.listen(3001);
+console.log("Web伺服器就緒，開始接受用戶端連線.");
+console.log("「Ctrl + C」可結束伺服器程式.");
+
+
+var mysql = require('mysql');
+const { application, response } = require("express");
+var connection = mysql.createConnection({
+	host : '127.0.0.1',
+	port : 8889,
+	user : 'root',
+	password : 'root',
+	database : 'nba'
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.log(JSON.stringify(err));
+		return;
+	}
+});
+
+
+
+app.get("/page/:start/:end", function (request, response) {
+	var start = parseInt(request.params.start);
+	var end = parseInt(request.params.end);
+	connection.query('select * from players order by name asc limit ?, ?', 
+		[start,end],
+		function(err, rows) {
+			if (err)	{
+				console.log(JSON.stringify(err));
+				return;
+			}
+			response.send(JSON.stringify(rows));
+		}
+	); 
+	})	
+
+	app.get("/chart", function (request, response) {
+		connection.query('SELECT team_name as "teamname" ,COUNT(*) as "count"  FROM players GROUP BY team_name;', 
+			'',
+			function(err, rows) {
+				if (err)	{
+					console.log(JSON.stringify(err));
+					return;
+				}
+				response.send(JSON.stringify(rows));
+			}
+		); 
+		})	
+	
+app.get("/players/all", function (request, response) {
+	console.log("name:");
+	connection.query('select * from players', 
+		'',
+		function(err, rows) {
+			if (err)	{
+				console.log(JSON.stringify(err));
+				return;
+			}
+			response.send(JSON.stringify(rows));
+		}
+	);  
+})
+
+app.get("/search/all/:name", function(request,response){
+	var name = request.params.name;
+    connection.query("select * from players where name like ?",
+	["%"+name+"%"],
+    function(error,rows){
+		console.log("name:"+name);
+		console.log("rows:"+ rows);
+		if(error){
+			console.log(error);
+            return;
+        }
+		response.send(rows);
+    })
+})
+
+app.get("/players/:teamacronym", function(request,response){
+	var teamacronym = request.params.teamacronym;
+    connection.query("select * from players where team_acronym = ?",
+    [teamacronym],
+    function(error,rows){
+		
+		if(error){
+			console.log(error);
+            return;
+        }
+		response.send(rows);
+    })
+})
+
+
+app.get("/players/:teamacronym/:name", function(request,response){
+	var teamacronym = request.params.teamacronym;
+	var name = request.params.name || "";
+    connection.query("select * from players where team_acronym = ? and name like ?",
+    [teamacronym,"%"+name+"%"],
+    function(error,rows){
+		console.log(name); 
+		if(error){
+			console.log(error);
+            return;
+        }
+		response.send(rows);
+    })
+})
+
+
---
+++ importData.js
@@ -0,0 +1,58 @@
+var userJSON = require('./players.json');
+
+let yo = [];
+ for(var x in userJSON){
+    let test =Object.keys(userJSON[x])
+
+    yo[x] = "("+
+'"'+userJSON[x].name+'"'+","+
+"'"+userJSON[x].team_acronym+"'"+","+
+"'"+userJSON[x].team_name+"'"+","+
+"'"+userJSON[x].games_played+"'"+","+
+"'"+userJSON[x].minutes_per_game+"'"+","+
+"'"+userJSON[x].field_goals_attempted_per_game+"'"+","+
+"'"+userJSON[x].field_goals_made_per_game+"'"+","+
+"'"+userJSON[x].field_goal_percentage+"'"+","+
+"'"+userJSON[x].free_throw_percentage+"'"+","+
+"'"+userJSON[x].three_point_attempted_per_game+"'"+","+
+"'"+userJSON[x].three_point_made_per_game+"'"+","+
+"'"+userJSON[x].three_point_percentage+"'"+","+
+"'"+userJSON[x].points_per_game+"'"+","+
+"'"+userJSON[x].offensive_rebounds_per_game+"'"+","+
+"'"+userJSON[x].defensive_rebounds_per_game+"'"+","+
+"'"+userJSON[x].rebounds_per_game+"'"+","+
+"'"+userJSON[x].assists_per_game+"'"+","+
+"'"+userJSON[x].steals_per_game+"'"+","+
+"'"+userJSON[x].blocks_per_game+"'"+","+
+"'"+userJSON[x].turnovers_per_game+"'"+","+
+"'"+userJSON[x].player_efficiency_rating+"'"+
+    ")";
+};   
+
+var mysql = require('mysql');
+const { use } = require('express/lib/application');
+const { CLIENT_LONG_PASSWORD } = require('mysql/lib/protocol/constants/client');
+var database = mysql.createConnection({
+	host : '127.0.0.1',
+	port : 9999,
+	user : 'root',
+	password : 'root',
+	database : 'nba'
+});
+ database.connect(function(connectionError){
+  if(connectionError){
+    throw connectionError;
+  }
+  for(i=0;i<userJSON.length;i++){
+    let sqlgo = "insert into players values" +" "+ yo[i]
+    database.query(sqlgo
+        ,(queryError)=>{if(queryError){
+          throw queryError;
+        }})
+  }
+  database.end((err)=>{
+      if(err){
+          console.log(err);
+      }
+  })
+}); 
